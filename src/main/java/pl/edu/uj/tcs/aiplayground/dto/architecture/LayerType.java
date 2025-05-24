7f85bc0179a6e8f4aaae58c7ee42c0906309b05e
Hubizon <hubert@jastrzebscy.org>
2025-05-25 00:02:04 +0200

    feat(LayerType): Add name parameter

--- src/main/java/pl/edu/uj/tcs/aiplayground/dto/architecture/LayerType.java
+++ src/main/java/pl/edu/uj/tcs/aiplayground/dto/architecture/LayerType.java
@@ -5,17 +5,24 @@ import pl.edu.uj.tcs.aiplayground.core.layers.*;
 import java.util.function.Function;
 
 public enum LayerType {
-    LINEAR(LinearParams.class, params -> new LinearLayer((LinearParams) params)),
-    RELU(EmptyParams.class, params -> new ReluLayer()),
-    SIGMOID(EmptyParams.class, params -> new SigmoidLayer()),
-    SOFTMAX(EmptyParams.class, params -> new SoftMaxLayer());
+    LINEAR("Linear Layer", LinearParams.class, params -> new LinearLayer((LinearParams) params)),
+    RELU("ReLU", EmptyParams.class, params -> new ReluLayer()),
+    SIGMOID("Sigmoid", EmptyParams.class, params -> new SigmoidLayer()),
+    SOFTMAX("Softmax", EmptyParams.class, params -> new SoftMaxLayer());
 
+    private final String name;
     private final Class<? extends LayerParams> paramType;
     private final Function<LayerParams, Layer> factory;
 
-    LayerType(Class<? extends LayerParams> paramType, Function<LayerParams, Layer> factory) {
+    LayerType(String name, Class<? extends LayerParams> paramType, Function<LayerParams, Layer> factory) {
+        this.name = name;
         this.paramType = paramType;
         this.factory = factory;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 
     public Class<? extends LayerParams> getParamType() {
