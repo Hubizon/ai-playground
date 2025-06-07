eccfe68c7a5ca5b61f41e67e503308ee536f803e
fimpro <filipmanijak05@gmail.com>
2025-06-07 11:15:33 +0200

    Insert example data

--- scripts/init_db.ps1
+++ scripts/init_db.ps1
@@ -4,4 +4,9 @@ psql -U postgres -f ".\db\init_pg.sql"
 
 psql -U aiplayground -d aiplayground -f ".\db\create.sql"
 
+psql -U aiplayground -d aiplayground -f ".\db\fill.sql"
+
+psql -U aiplayground -d aiplayground -f ".\db\triggers.sql"
+
+
 Write-Host "Done. User: aiplayground | Password: aiplayground | Database: aiplayground"
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -6,4 +6,9 @@ sudo -u postgres psql -f ./db/init_pg.sql
 
 psql -U aiplayground -d aiplayground -f ./db/create.sql
 
+psql -U aiplayground -d aiplayground -f .\db\fill.sql
+
+psql -U aiplayground -d aiplayground -f .\db\triggers.sql
+
+
 echo "Done. User: aiplayground | Password: aiplayground | Database: aiplayground"
